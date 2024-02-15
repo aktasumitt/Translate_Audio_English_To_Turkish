0aa49d858ccaccd390052a7798e782fce79a22ea
Ümit Aktaş <152315417+aktasumitt@users.noreply.github.com>
2024-02-15 16:48:12 +0300

    Update

--- Speech_Recognation/config.py
+++ Speech_Recognation/config.py
@@ -1,3 +1,3 @@
-Audio_Path="C:\\Users\\umtak\\My_Models\\Translate_Audio_English_To_Turkish\\Speech_Recognation\\New_York_City.mp3"
-Save_Path="C:\\Users\\umtak\\My_Models\\Translate_Audio_English_To_Turkish\\Speech_Recognation\\Predicts\\text_pred1.txt"
+Audio_Path="Speech_Recognation\\New_York_City.mp3"
+Save_Path="Speech_Recognation\\Predicts\\text_pred1.txt"
 SAVE=True
